dbc377f21cfd118738411f87cddda4fbbf587b60
Akshat Srivastava <srivastavaakshat52@gmail.com>
2025-07-06 23:02:11 +0530

    Update axios baseURL for local and production environments

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,16 @@
 import axios from "axios";
 
 const api = axios.create( {
-    baseURL: "http://localhost:5001/api"
+    baseURL: window.location.hostname === "localhost"      // we are checking that we are locally  
+    ? "http://localhost:5001/api"
+    : "https://leafpad.onrender.com/api"
 });
 
 export default api;
+
+
+// window.location.hostname => This is JavaScript running in the browser. It gives you the domain name in the address bar.
+
+// When you run locally:   =>   window.location.hostname = localhost
+
+// When you deploy to Vercel:   =>  window.location.hostname = your-vercel-domain.com
